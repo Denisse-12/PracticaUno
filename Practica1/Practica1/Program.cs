68716a7d36a70c6dc3a5b1893ea8becaaf4ef00b
Denisse-12 <denisse.4567@outlook.com>
2016-09-07 18:18:53 -0700

    Programa en Visual

---
+++ Practica1/Practica1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float Sueldo;
+            string linea;
+            Console.Write("Ingrese el sueldo:");
+            linea = Console.ReadLine();
+            Sueldo = float.Parse(linea);
+            if(Sueldo>300)
+            {
+                Console.Write("Esta persona debe abonar");
+            }
+            Console.ReadKey();
+        }
+    }
+}
